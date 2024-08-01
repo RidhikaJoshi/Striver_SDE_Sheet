6724a1fd1ec69ce1fe8fd96519ef0077b2b3380d
RIDHIKA JOSHI <ridhikasmrh@gmail.com>
2024-08-01 11:42:26 +0530

    chore: Add implementation for stack using arrays

---
+++ Stack and Queue/Implement_Stack_using_Arrays.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class MyStack
+{
+private:
+    int arr[1000];
+    int top;
+public:
+    MyStack(){top=-1;}
+    int pop();
+    void push(int);
+};
+
+void MyStack ::push(int x) {
+    top+=1;
+   arr[top]=x;
+}
+
+// Function to remove an item from top of the stack.
+int MyStack ::pop() {
+   if(top==-1)
+        return -1;
+  
+    int topElement=arr[top];
+    top--;
+    return topElement;
+
+}
